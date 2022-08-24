6356b25a2616975065c466455a4b54b84eda7388
Ceci <admin@cecilias.me>
2022-08-23 19:23:34 -0500

    fix: preparation for XIV 6.2 release
    - updated isEmoteUnlocked pointer

--- Puppets/Utils/Emotes.cs
+++ Puppets/Utils/Emotes.cs
@@ -20,7 +20,7 @@ namespace Puppets.Utils
             {
                 if (Emotes.__isEmoteUnlocked == null)
                 {
-                    if (PuppetsPlugin.TargetScanner.TryScanText("E8 ?? ?? ?? ?? 84 C0 74 A4", out var emoteUnlockedPtr))
+                    if (PuppetsPlugin.TargetScanner.TryScanText("E9 ?? ?? ?? ?? 8B 13 41 B8", out var emoteUnlockedPtr))
                     {
                         PluginLog.Information($"emoteUnlockedPtr: {emoteUnlockedPtr:X}");
                         Emotes.__isEmoteUnlocked = Marshal.GetDelegateForFunctionPointer<IsEmoteUnlockedDelegate>(emoteUnlockedPtr);
